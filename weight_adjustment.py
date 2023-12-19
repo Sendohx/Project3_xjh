ab3cf54e49313419279298c1bbe799f6b6929efe
Sendohx <151484757+Sendohx@users.noreply.github.com>
2023-12-19 16:09:20 +0800

    Update weight_adjustment.py

--- weight_adjustment.py
+++ weight_adjustment.py
@@ -27,3 +27,15 @@ def weight_adjustment(asset_data):
     asset_data['weighted_return'] = asset_data['return'] * asset_data['weight']
 
     return asset_data
+
+def weight_describe(asset_data):
+    """
+    计算仓位均值及调仓频率
+    :param asset_data
+    """
+    mean_weight = asset_data['weight'].mean()
+
+    position_changes = asset_data['weight'].diff() != 0
+    adjustment_frequency = position_changes.sum() / len(asset_data)
+
+    return f"仓位均值: {mean_weight}, 调仓频率: {adjustment_frequency}"
